7e86a69c7759193b730288fb3ffbbf0c10ee8a0b
Johan Millberg <johan.millberg@kidbrooke.com>
2022-12-08 17:08:02 +0100

    jepp

--- Day8.cs
+++ Day8.cs
@@ -6,10 +6,15 @@ public class DayEight: IDay
 {
     public int partOne(string filePath)
     {
-        var result = File.ReadAllText(filePath).Split("\r\n")
-                                               .SelectMany((l, i) => l.Split()
-                                                                      .Select(s => new int[] {i, int.Parse(i.ToString())})
-                                                                      .ToArray());
+        int[][] inputMatrix = File.ReadAllText(filePath).Split("\r\n")
+                                                        .Select(l => l.ToArray()
+                                                        .Select(num => int.Parse(num.ToString()))
+                                                        .ToArray())
+                                                        .ToArray();
+
+        var coords = Enumerable.Range(0, inputMatrix[0].Length).Select(x => {x, })
+        foreach (var coords in Enumerable.Range(0, inputMatrix[0].Lengt)
+
         // foreach column
         return 1;
     }
@@ -19,10 +24,14 @@ public class DayEight: IDay
         return 1;
     }
 
-
-    private int countVisibleTrees(int[] line)
+    private bool isVisible(int[][] matrix, int x, int y)
     {
-        return 1;
+        bool visibleFromLeft = Enumerable.Range(0, x+1).Select(i => matrix[i][y]).Reverse().Skip(1).All(n => n < matrix[x][y]);
+        bool visibleFromTop = Enumerable.Range(0, y+1).Select(i => matrix[x][i]).Reverse().Skip(1).All(n => n < matrix[x][y]);
+        bool visibleFromRight = Enumerable.Range(x, matrix[0].Length - x).Select(i => matrix[i][y]).Reverse().Skip(1).All(n => n < matrix[x][y]);
+        bool visibleFromBot = Enumerable.Range(y, matrix[0].Length - y).Select(i => matrix[x][i]).Reverse().Skip(1).All(n => n < matrix[x][y]);
+
+        return visibleFromBot || visibleFromLeft || visibleFromRight || visibleFromTop;
     }
 
 }
